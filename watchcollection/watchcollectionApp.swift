f0e7c4a9d5657ab4c91e1db032df1bc585c632a6
Thalys Guimarães <eu@thalysguimaraes.com>
2025-12-20 10:29:14 -0300

    Initial Commit

---
+++ watchcollection/watchcollectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  watchcollectionApp.swift
+//  watchcollection
+//
+//  Created by Thalys Guimarães on 20/12/25.
+//
+
+import SwiftUI
+
+@main
+struct watchcollectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
